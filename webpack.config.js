8a24e80b31c69420fc1bb84f3cd3e90106b1d7fd
YoonJunHyeon <qmdldos@gmail.com>
2023-11-22 16:36:52 +0900

    config Fix : babel-loader module 커스터마이징

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js' // 번들 파일 이름
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -25,6 +27,14 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+            //babel-loader 세팅
+            {
+                test: /\.js$/, // .js 확장자로 끝나는 모든 파일
+                exclude: /node_modules/, // node_modules 폴더 제외
+                use: {
+                  loader: 'babel-loader', // babel-loader를 사용하여 변환
+                }
+            }
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
