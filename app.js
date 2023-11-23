42f647e144f2c8eaa7d0159659ae676d9fce9c14
YoonJunHyeon <qmdldos@gmail.com>
2023-11-23 19:38:17 +0900

    feat : jsx 없이 함수형 컴포넌트 작성해보기

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const test = Array.from("안녕하세요");
-console.log(test);
+// jsx없이 함수형 컴포넌트 작성하기
+import React, {useState} from 'react';
+
+const App = ()=>{
+  const [count, setCount]=useState(0);
+
+  return React.createElement(
+    'div', 
+    null,
+    React.createElement('p',{},`클릭 횟수 : ${count}`),
+    React.createElement('button',{onclick:()=>{setCount(count+1)}},"누르세용")
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+// jsx없이 함수형 컴포넌트 작성하기
+import React, {useState} from 'react';
+
+const App = ()=>{
+  const [count, setCount]=useState(0);
+
+  return React.createElement(
+    'div', 
+    null,
+    React.createElement('p',{},`클릭 횟수 : ${count}`),
+    React.createElement('button',{onclick:()=>{setCount(count+1)}},"누르세용")
+  );
+};
+
+export default App;
